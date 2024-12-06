64cebdc2e44dbce41455042a9921beaa7cd06079
csh <csh4170@mteg.co.kr>
2024-12-06 02:10:36 +0000

    test4

--- train_and_test/gru_inference_csv.py
+++ train_and_test/gru_inference_csv.py
@@ -12,10 +12,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 
 
-
-
-
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
